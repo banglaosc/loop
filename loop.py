8dac11e4f5cdd10480d251f11f5a1f3ec8719e03
jubelAhmed <jubel35-1648@diu.edu.bd>
2020-06-21 11:55:29 +0600

    first commit

---
+++ loop.py
@@ -0,0 +1,46 @@
+    
+# name print 10 times
+for i in range(10):
+    print( str(i) + " : my name is jhon")
+
+# print 10 times with start and before ending 
+for i in range(5,20):
+    print(i)
+
+
+# number print single
+arr = [10,12,34,45,56]
+
+for value in arr:
+    print(value)
+    # print("the number is "+ str(value))
+
+
+#  favourt food list with range and lenght example
+# food = ["icecream","chocklate","juice","Burger"]
+# list_length = len(food)
+# print("total food item : "+ str(list_length))
+
+# for position in range(list_length):
+#     print(food[position])
+
+# reduce list value
+# arr = [10,12,34,45,56]
+
+# for value in arr:
+#     print(value-2)
+
+
+#  summation
+
+# arr = [10,12,34,45,56]
+
+# sum = 0
+# for value in arr:
+#     sum = sum + value
+
+# print("total sum is "+str(sum))
+
+
+
+
